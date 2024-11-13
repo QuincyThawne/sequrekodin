d58cf26df8a576cefdc41631f4aebabe139cf464
Quincy Thawne <142000861+QuincyThawne@users.noreply.github.com>
2024-11-13 20:44:58 +0530

    Update mitgStratVuln.c

--- Mitigation Strategies/mitgStratVuln.c
+++ Mitigation Strategies/mitgStratVuln.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+void vuln_function(int **ptr)
+{
+    int *local_ptr=*ptr;
+    free(local_ptr);
+    *ptr=NULL;
+}
+
 int main() {
  int *ptr = (int*)malloc(sizeof(int)); // Allocate memory
  *ptr = 42; // Assign a value to the allocated memory
- printf("Value: %d\n", *ptr);
- free(ptr); 
- printf("Value after free: %d\n", *ptr); // Use-After-Free
- *ptr = 100; // Writing to freed memory
- // Program may still proceed, leading to undefined behavior
- printf("New value: %d\n", *ptr); // This could crash the program or give garbage
+ printf("Before Vulnerability: *ptr= %d\n", *ptr);
+ vuln_function(&ptr); 
+ printf("After Vulnerability: *ptr= %d (Undefined behavior)\n", *ptr); // Use-After-Free
+ if(ptr==NULL)
+    printf("Pointer Successfully handled.\n");
+ else
+    printf("Vulnerability: Dangling pointer exists.\n");
  return 0;
 }
